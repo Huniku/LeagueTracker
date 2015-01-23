ce01db4ff09c64dc9ee82e38b951246a44c72382
Ben White <ben.white@disney.com>
2015-01-22 18:57:45 -0800

    Changing players to arrays (and changing field name to teamX) to support multiplayer games

--- schema/GameModel.js
+++ schema/GameModel.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GameSchema = new Schema({
-    player0: { type: String, required: true },
-    player1: { type: String, required: true },
+    team0: [{ type: String, required: true }],
+    team1: [{ type: String, required: true }],
     winner: { type: Number, min: 0, max: 1 },
     league: { type: Schema.ObjectId, ref: 'League'},
     season: { type: Schema.ObjectId, ref: 'Season'},
