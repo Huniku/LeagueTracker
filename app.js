bd776769ad5bde28d28fc737b3207b9485edcdf7
Ben White <aussiewidget@gmail.com>
2015-10-18 22:49:39 -0700

    Updating the startup

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(function(req, res, next) {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
+    console.log("DB connection opened");
     app.get('/', home.index);
     app.get('/users/login', users.login);
     app.post('/users/login', users.attemptLogin);
@@ -79,13 +80,13 @@ db.once('open', function callback () {
 // development error handler
 // will print stacktrace
 //if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
 //}
 
 // production error handler
